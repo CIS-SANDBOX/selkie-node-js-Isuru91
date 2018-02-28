fa3886e1ff6cbb4d682c18650f63cad42a270de5
Isuru Edirisinghe <isuru991@gmail.com>
2018-02-28 21:35:43 +0530

    Server up and running

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-//index file
+var express = require('express');
+var app = express();
+
+app.get('/api', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(8090, function () {
+  console.log('selkie-node app listening on port 8090!');
+});
